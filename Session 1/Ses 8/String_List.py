a1a6848964d8666cbf93981b1ec85cf10d1f8f31
Tony Tran <52320344+khanhtran25@users.noreply.github.com>
2019-07-03 12:13:16 +0700

    Delete String_List.py

--- Session 1/Ses 8/String_List.py
+++
@@ -1,9 +0,0 @@
-name = 'com rang'
-# characters = list(name)
-# print(name)
-# print(characters)
-
-name_string = 'hardwell, kanzaxx, kshmr'
-print(name_string)
-names = name_string.split(",")
-print(names)
